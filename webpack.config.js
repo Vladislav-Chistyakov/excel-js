499fa709f4266b9e897e3f993b9e7270e307a9a6
Vladislav_Chistyakov <vladischis@gmail.com>
2023-09-25 19:27:10 +0300

    add favicon, alias, plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -8,5 +10,28 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
-    }
+    },
+    resolve: {
+        // '../path/file.js'
+        // '../path/file'
+        extensions: ['.js'],
+        // '../../path/file'
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src', 'core')
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ],
+        })
+    ]
 }
